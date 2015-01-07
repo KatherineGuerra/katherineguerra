94118b254cb2bc830f0fcd56445216318a5e2474
katherine guerra <guerrakn@.gmail>
2015-01-07 11:29:04 -0800

    commit

---
+++ public_html/script.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$("document").ready(function(){
+    $(".umm").css("background-color", "cyan");
+    $(".1").css("background-color", "pink", "color", "white");
+    $(".2").css("background-color", "gray", "color", "pink");
+});
