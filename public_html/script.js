ba83e1540503413c8d8fbedcef5467c79999d14b
katherine guerra <guerrakn@.gmail>
2015-01-08 11:27:44 -0800

    commit

--- public_html/script.js
+++ public_html/script.js
@@ -6,7 +6,15 @@
 
 
 $("document").ready(function(){
-    $(".umm").css("background-color", "cyan");
-    $(".1").css("background-color", "pink", "color", "white");
-    $(".2").css("background-color", "gray", "color", "pink");
+    $(".umm").css("background-color","cyan");
+    $(".1").css({"background-color": "pink", "color": "blue"});
+    $(".2").css({"background-color": "gray", "color": "white"});
+    $('.logo').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
 });
+ function mouseOverMe(){
+   $("#second").html("you put your cursor on my logo");
+ }
+ function mouseOutMe(){
+   $("#second").html("you stopped touching my logo");  
+ }
+ 
